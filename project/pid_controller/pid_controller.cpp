2432bc914cc41f5c2db8f57df8dce11a2ea506a4
Neha Verma <nehbehl@gmail.com>
2021-08-26 18:38:27 +0530

    Update pid_controller.cpp

--- project/pid_controller/pid_controller.cpp
+++ project/pid_controller/pid_controller.cpp
@@ -19,6 +19,16 @@ void PID::Init(double Kpi, double Kii, double Kdi, double output_lim_maxi, doubl
    /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    **/
+  
+  Kp = Kpi;
+  Ki = Kii;
+  Kd = Kdi;
+  output_lim_maxi = 0.0;
+  output_lim_mini = 0.0;
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
+  cte = 0.0;
 }
 
 
@@ -26,6 +36,10 @@ void PID::UpdateError(double cte) {
    /**
    * TODO: Update PID errors based on cte.
    **/
+  d_error = Kd * (cte - this->cte)/delta_time;
+  p_error = Kp * cte;
+  i_error += Ki * cte * delta_time;
+  this->cte = cte;
 }
 
 double PID::TotalError() {
@@ -33,7 +47,12 @@ double PID::TotalError() {
    * TODO: Calculate and return the total error
     * The code should return a value in the interval [output_lim_mini, output_lim_maxi]
    */
-    double control;
+   
+    double control= p_error + i_error + d_error;
+    if(control > output_lim_maxi)
+      control = output_lim_maxi;
+    else
+      control = output_lim_mini;
     return control;
 }
 
@@ -41,4 +60,5 @@ double PID::UpdateDeltaTime(double new_delta_time) {
    /**
    * TODO: Update the delta time with new value
    */
+  delta_time=new_delta_time;
 }
